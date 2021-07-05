8fe3a7a8b11115d4af2621a3f70f40bca351796e
Dominic Swaine <dominic.swaine@gmail.com>
2021-07-05 23:26:13 +0100

    Complete lesson 4 (Challenge: Nested Scrolling Stacks)

--- Lesson-04/Layout/ContentView.swift
+++ Lesson-04/Layout/ContentView.swift
@@ -1,35 +1,3 @@
-/// Copyright (c) 2020 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
-
 import SwiftUI
 
 struct ContentView: View {
@@ -37,7 +5,35 @@ struct ContentView: View {
 
   var body: some View {
     NavigationView {
-      Divider()
+      ScrollViewReader { proxy in
+        ScrollView(.vertical) {
+          let horizontalPadding: CGFloat = 40
+          LazyVStack(alignment: .leading) {
+            ForEach(Genre.list) { genre in
+              Text(genre.name)
+                .fontWeight(.heavy)
+                .padding(.leading, horizontalPadding)
+                .id(genre)
+              
+              ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack(spacing: 20) {
+                  ForEach(genre.subgenres, content: \.view)
+                }
+                .padding(.leading, horizontalPadding)
+              }
+              
+              Divider()
+                .padding(.leading, horizontalPadding)
+                .padding(.top)
+            }
+          }
+          .onChange(of: selectedGenre) { genre in
+            withAnimation {proxy.scrollTo(genre, anchor: .top)}
+            selectedGenre = nil
+          }
+        }
+      }
+      .padding(.top)
       .navigationBarTitleDisplayMode(.inline)
       .toolbar {
         ToolbarItem {
